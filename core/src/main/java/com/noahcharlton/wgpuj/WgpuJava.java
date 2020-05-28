693f41df8d6f0df592940536ba1b5c6d80fee9d4
Noah Charlton <ncharlton002@gmail.com>
2020-05-28 14:05:31 -0400

    Initial java project

---
+++ core/src/main/java/com/noahcharlton/wgpuj/WgpuJava.java
@@ -0,0 +1,7 @@
+package com.noahcharlton.wgpuj;
+
+public class WgpuJava {
+
+    public static final String VERSION = "0.1.0";
+
+}
---
+++ examples/src/main/java/com.noahcharlton.wpguj.examples/Example.java
@@ -0,0 +1,11 @@
+package com.noahcharlton.wpguj.examples;
+
+import com.noahcharlton.wgpuj.WgpuJava;
+
+public class Example {
+
+    public static void main(String[] args){
+        System.out.println("Version: " + WgpuJava.VERSION);
+    }
+
+}
